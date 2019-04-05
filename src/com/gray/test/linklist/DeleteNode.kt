a21514c43fd7f815971053171503bac456938eec
gray <emellend@gmail.com>
2019-04-05 19:12:28 +0800

    $(链表): 删除链表中的节点

---
+++ src/com/gray/test/linklist/DeleteNode.kt
@@ -0,0 +1,48 @@
+package com.gray.test.linklist
+
+/**
+ * 删除链表中的节点
+ *
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ *
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ *
+ * 示例 1:
+ *      输入: head = [4,5,1,9], node = 5
+ *      输出: [4,1,9]
+ *      解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ *
+ * 示例 2:
+ *      输入: head = [4,5,1,9], node = 1
+ *      输出: [4,5,9]
+ *      解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *
+ * 说明:
+ *      链表至少包含两个节点。
+ *      链表中所有节点的值都是唯一的。
+ *      给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ *      不要从你的函数中返回任何结果。
+ * */
+data class ListNode(var value: Int, var next: ListNode? = null)
+
+fun deleteNode(node: ListNode) {
+    val next = node.next!!
+    node.value = next.value
+    node.next = next.next
+}
+
+fun main() {
+    val head = ListNode(4)
+    val node1 = ListNode(5)
+    val node2 = ListNode(1)
+    val node3 = ListNode(9)
+    head.next = node1
+    node1.next = node2
+    node2.next = node3
+    deleteNode(node1)
+    var node: ListNode? = head
+    while (node != null) {
+        println(node.value)
+        node = node.next
+    }
+}
