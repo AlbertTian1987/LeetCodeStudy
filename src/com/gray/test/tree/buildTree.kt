a6bc119357071899f71b89bac9347d448ed22be5
tgh <tianguohui@changba.com>
2019-04-22 15:06:20 +0800

    $(树): 从前序与中序遍历序列构造二叉树

--- src/com/gray/test/tree/buildTree.kt
+++ src/com/gray/test/tree/buildTree.kt
@@ -21,35 +21,54 @@ package com.gray.test.tree
  */
 
 fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
-    if (preorder.isEmpty()) {
-        return null
-    }
-    val n = inorder.size - 1
-    return build(0, 0, n, n, preorder, inorder)
+    return build(preorder, inorder, 0, 0, inorder.size - 1)
 }
-
 /**
- * 前序中左起第一位1肯定是根结点，我们可以据此找到中序中根结点的位置rootin；
- * 中序中根结点左边就是左子树结点，右边就是右子树结点，即[左子树结点，根结点，右子树结点]，我们就可以得出左子树结点个数为int left = rootin - leftin;；
- * 前序中结点分布应该是：[根结点，左子树结点，右子树结点]；
- * 根据前一步确定的左子树个数，可以确定前序中左子树结点和右子树结点的范围；
- * 如果我们要前序遍历生成二叉树的话，下一层递归应该是：
- * 左子树：root->left = pre_order(前序左子树范围，中序左子树范围，前序序列，中序序列);；
- * 右子树：root->right = pre_order(前序右子树范围，中序右子树范围，前序序列，中序序列);。
- * 每一层递归都要返回当前根结点root；
+ * 思路：
+ *       a
+ *      / \
+ *     b   c
+ *    / \   \
+ *   d   e   f
+ *          / \
+ *         g   h
+ *
+ * 前序遍历: a->b->d->e->c->f->g->h
+ *       root| 左子树 ||  右子树  |
+ * 中序遍历: d->b->e->a->c->g->f->h
+ *         |左子树 |root|  右子树  |
+ *
+ * 1. 先序遍历的第一个是root结点
+ * 2. 找到root结点在中序遍历里的位置
+ * 3. 中序遍历root左边是左子树，得到它的长度
+ * 4. 可以确定先序遍历里右子树起始位置
+ * 5. 重复1-4
+ *
+ * @param preorder 完整的先序遍历
+ * @param inorder  完整的中序遍历
+ * @param preSt    子树先序遍历起始点
+ * @param inSt     子树中序遍历起始点
+ * @param inEnd    子树中序遍历结束点
+ * @return
  */
-fun build(leftpre: Int, leftin: Int, rightpre: Int, rightin: Int, preorder: IntArray, inorder: IntArray): TreeNode? {
-    if (leftpre > rightpre || leftin > rightin) {
+fun build(preorder: IntArray, inorder: IntArray, preSt: Int, inSt: Int, inEnd: Int): TreeNode? {
+
+    if (preSt > preorder.size - 1 || inSt > inEnd) {
         return null
     }
-    val root = TreeNode(preorder[leftpre])
-    var rootin = leftin
-    while (rootin <= rightin && root.`val` != inorder[rootin]) {
-        rootin++
+    val root = TreeNode(preorder[preSt])
+    //根节点在中序遍历中的index
+    var rootIndex = inSt
+    while (rootIndex <= inEnd && root.`val` != inorder[rootIndex]) {
+        rootIndex++
     }
-    val left = rootin - leftin
-    root.left = build(leftpre + 1, leftin, leftpre + left, rootin - 1, preorder, inorder)
-    root.right = build(leftpre + left + 1, rootin + 1, rightpre, rightin, preorder, inorder)
+    //左子树的长度
+    val leftLength = rootIndex - inSt
+    //右子树在先序遍历的起始点
+    val rightSt = preSt + leftLength + 1
+    val leftSt = preSt + 1
+    root.left = build(preorder, inorder, leftSt, inSt, rootIndex - 1)
+    root.right = build(preorder, inorder, rightSt, rootIndex + 1, inEnd)
     return root
 }
 
