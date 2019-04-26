33e848d5eef69106ca0b290a01c644c59c446c76
gray <emellend@gmail.com>
2019-04-26 22:48:02 +0800

    $(回溯算法): 全排列

--- src/com/gray/test/backTracking/permute.kt
+++ src/com/gray/test/backTracking/permute.kt
@@ -19,56 +19,28 @@ package com.gray.test.backTracking
  *      [3,2,1]
  *  ]
  * */
-import java.util.*
-
 fun permute(nums: IntArray): List<List<Int>> {
-    fun help(nums: List<Int>): List<List<Int>> {
-        if (nums.size == 1) {
-            val ret = ArrayList<List<Int>>(1)
-            ret.add(nums)
-            return ret
-        }
-        val preResult = help(nums.slice(0 until nums.size - 1))
-        val result = ArrayList<List<Int>>((preResult[0].size + 1) * preResult.size)
-        val newNum = nums.last()
-        for (preList in preResult) {
-            repeat(preList.size + 1) {
-                val list = ArrayList<Int>(preList.size + 1)
-                list.addAll(preList)
-                list.add(it, newNum)
-                result.add(list)
-            }
-        }
-        return result
-    }
-    return help(nums.toList())
-}
-
-fun permute2(nums: IntArray): List<List<Int>> {
-    val n = nums.size
-    val unvisited = BooleanArray(n) { true }
-    val path = IntArray(n)
-    val result = ArrayList<List<Int>>(n * (n - 1))
-
-    fun dfs(nums: IntArray, step: Int) {
+    val used = BooleanArray(nums.size)
+    val path = IntArray(nums.size)
+    val result = ArrayList<List<Int>>()
+    fun dfs(step: Int) {
         if (step == nums.size) {
             result.add(path.toList())
             return
         }
         for (i in 0 until nums.size) {
-            if (unvisited[i]) {
+            if (!used[i]) {
                 path[step] = nums[i]
-                unvisited[i] = false
-                dfs(nums, step + 1)
-                unvisited[i] = true
+                used[i] = true
+                dfs(step + 1)
+                used[i] = false
             }
         }
     }
-    dfs(nums, 0)
+    dfs(0)
     return result
 }
 
 fun main() {
     println(permute(intArrayOf(1, 2, 3)))
-    println(permute2(intArrayOf(1, 2, 3)))
 }
