229a007a05ae2329a212426aa229667b55a69be7
gray <emellend@gmail.com>
2019-04-29 15:09:02 +0800

    $(排序): 插入排序

--- src/com/gray/test/sortAndSearch/insertSort.kt
+++ src/com/gray/test/sortAndSearch/insertSort.kt
@@ -10,16 +10,12 @@ fun insertSort(array: IntArray) {
     var j = 1
     while (j < array.size) {
         val value = array[j]
-        var insertIndex = j
-        while ((insertIndex - 1) >= 0 && array[insertIndex - 1] > value) {
-            insertIndex--
+        var i = j - 1
+        while (i >= 0 && array[i] > value) {
+            array[i + 1] = array[i]
+            i--
         }
-        if (insertIndex != j) {
-            for (i in j downTo insertIndex + 1) {
-                array[i] = array[i - 1]
-            }
-            array[insertIndex] = value
-        }
+        array[i + 1] = value
         j++
     }
 }
