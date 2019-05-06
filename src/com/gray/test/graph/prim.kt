5dbc89a4e9f02ff3494156d8826037251217faaa
gray <emellend@gmail.com>
2019-05-06 11:38:22 +0800

    $(图): prim

--- src/com/gray/test/graph/prim.kt
+++ src/com/gray/test/graph/prim.kt
@@ -10,52 +10,41 @@ import java.util.*
  *
  * 算法时间复杂度O(n^2)
  */
-fun prim(g2: G2) {
-    val V = g2.V
-    val E = g2.E
-    val W = g2.W
-    val S = ArrayList<Int>(V.size)
-    val choosedE = Array(E.size) { BooleanArray(E.size) }
-    S.add(V[0])
-    while (S.size != V.size) {
-        var minW = Int.MAX_VALUE
-        var v1 = 0
-        var v2 = 0
-        for (s in S) {
-            for (e in E[s]) {
-                if (S.contains(e)) {
-                    continue
-                }
-
-                if (choosedE[s][e] || choosedE[e][s]) {
-                    continue
-                }
-                if (W[s][e] < minW) {
-                    minW = W[s][e]
-                    v1 = s
-                    v2 = e
+fun prim(vertexCount: Int, adjacentEdges: HashMap<Int, Array<Edge>>) {
+    val visited = BooleanArray(vertexCount)
+    visited[0] = true
+    repeat(vertexCount - 1) {
+        var minDist = Int.MAX_VALUE
+        var begin = -1
+        var end = -1
+        for (u in 0 until vertexCount) {
+            val edge = adjacentEdges[u] ?: continue
+            if (visited[u]) {
+                val e = edge.filter { !visited[it.end] }.sortedBy { it.weight }.firstOrNull()
+                e?.let {
+                    if (e.weight < minDist) {
+                        minDist = e.weight
+                        begin = e.begin
+                        end = e.end
+                    }
                 }
             }
         }
-        choosedE[v1][v2] = true
-        choosedE[v2][v1] = true
-        if (S.contains(v1)) {
-            S.add(v2)
-        } else {
-            S.add(v1)
-        }
-    }
-    choosedE.forEachIndexed { i, array ->
-        array.forEachIndexed { j, b ->
-            if (b) {
-                if (i < j) {
-                    println("choose edge($i,$j) w=${W[i][j]}")
-                }
-            }
-        }
+        println("choose edge(${begin + 1},${end + 1}) w = $minDist")
+        visited[end] = true
     }
 }
 
 fun main() {
-    prim(genG2())
+    val adjacentEdges = HashMap<Int, Array<Edge>>()
+    adjacentEdges[0] = arrayOf(Edge(0, 1, 6), Edge(0, 2, 1), Edge(0, 3, 5))
+    adjacentEdges[1] = arrayOf(Edge(1, 0, 6), Edge(1, 2, 5), Edge(1, 4, 3))
+    adjacentEdges[2] = arrayOf(
+            Edge(2, 0, 1), Edge(2, 1, 5), Edge(2, 3, 5),
+            Edge(2, 4, 6), Edge(2, 5, 4)
+    )
+    adjacentEdges[3] = arrayOf(Edge(3, 0, 5), Edge(3, 2, 5), Edge(3, 5, 2))
+    adjacentEdges[4] = arrayOf(Edge(4, 1, 3), Edge(4, 2, 6), Edge(4, 5, 6))
+    adjacentEdges[5] = arrayOf(Edge(5, 2, 4), Edge(5, 3, 2), Edge(5, 4, 6))
+    prim(6, adjacentEdges)
 }
