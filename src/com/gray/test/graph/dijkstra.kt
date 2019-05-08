ff7207a66ccc3c0a06d5c43e52e73e3e91bf037f
gray <emellend@gmail.com>
2019-05-08 17:53:49 +0800

    $(图): dijkstra

--- src/com/gray/test/graph/dijkstra.kt
+++ src/com/gray/test/graph/dijkstra.kt
@@ -13,40 +13,26 @@ import java.util.*
  * */
 
 fun dijkstra(origin: Int, vertexCount: Int, adjacentEdges: HashMap<Int, ArrayList<Edge>>) {
-    val sptSet = BooleanArray(vertexCount) { false }
+    val visited = BooleanArray(vertexCount) { false }
     val dist = IntArray(vertexCount) { Int.MAX_VALUE }
     dist[origin] = 0
     repeat(vertexCount - 1) {
-        val u = findMinDistance(vertexCount, sptSet, dist)
-        sptSet[u] = true
-        for (v in 0 until vertexCount) {
-            val edges = adjacentEdges[u] ?: continue
-            if (!sptSet[v] && dist[u] != Int.MAX_VALUE) {//u不在集合S里，且U目前是可达的
-                val d = edges.firstOrNull { it.end == v }//从u能够达到v吗？
-                d?.let {
-                    if (it.weight + dist[u] < dist[v]) {//源点到u的距离+u到v的距离，就是源点到v的距离
-                        dist[v] = it.weight + dist[u]
+        //找不在集合里的距离最小顶点
+        val u = (0 until vertexCount).filter { !visited[it] }
+                .sortedBy { dist[it] }.first()
+        //将他加入集合
+        visited[u] = true
+        //更新其他不再集合里的顶点的距离
+        adjacentEdges[u]!!.asSequence()
+                .filter { !visited[it.end] }
+                .forEach {
+                    if (dist[u] + it.weight < dist[it.end]) {
+                        dist[it.end] = dist[u] + it.weight
                     }
                 }
-            }
-        }
+
     }
     println(Arrays.toString(dist))
-}
-
-/**
- * 找不在sptSet集合里，距离最小的顶点编号
- */
-fun findMinDistance(vertexCount: Int, sptSet: BooleanArray, dist: IntArray): Int {
-    var minDist = Int.MAX_VALUE
-    var minIndex = -1
-    for (i in 0 until vertexCount) {
-        if (!sptSet[i] && dist[i] <= minDist) {
-            minDist = dist[i]
-            minIndex = i
-        }
-    }
-    return minIndex
 }
 
 fun main() {
