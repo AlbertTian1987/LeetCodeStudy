8c3c721d09982354d3d279f44bf0f1205dbec043
gray <emellend@gmail.com>
2019-05-11 14:41:16 +0800

    $(树): 路径总和 III

---
+++ src/com/gray/test/tree/pathSum.kt
@@ -0,0 +1,58 @@
+package com.gray.test.tree
+
+import java.util.*
+
+/**
+ * 437. 路径总和 III
+ * 给定一个二叉树，它的每个结点都存放着一个整数值。
+ * 找出路径和等于给定数值的路径总数。
+ * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+ *
+ * 示例：
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ *      10
+ *     /  \
+ *    5   -3
+ *   / \    \
+ *  3   2   11
+ * / \   \
+ * 3  -2   1
+ *
+ * 返回 3。和等于 8 的路径有:
+ *
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3.  -3 -> 11
+ * */
+
+fun pathSum(root: TreeNode?, sum: Int): Int {
+    val valueList = ArrayList<Int>()
+    var ret = 0
+    fun dfs(root: TreeNode?, valueList: ArrayList<Int>) {
+        if (root == null) {
+            return
+        }
+        valueList.add(root.`val`)
+        if (valueList.isNotEmpty()) {
+            val lastIndex = valueList.lastIndex
+            for (i in 0 until valueList.size) {
+                val slice = valueList.slice(i..lastIndex)
+                if (slice.sum() == sum) {
+                    println(slice)
+                    ret++
+                }
+            }
+        }
+        dfs(root.left, valueList)
+        dfs(root.right, valueList)
+        valueList.removeAt(valueList.size - 1)
+    }
+    dfs(root, valueList)
+    return ret
+}
+
+fun main() {
+    val node = createTreeNode(linkedListOf(arrayListOf(10, 5, -3, 3, 2, null, 11, 3, -2, null, 1, null, null)))
+    pathSum(node, 8)
+}
