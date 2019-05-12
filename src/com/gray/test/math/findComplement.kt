d75fe98a984d7b3b697b1d093a47a8a0561495b6
gray <emellend@gmail.com>
2019-05-12 14:46:13 +0800

    $(数学): 数字的补数

---
+++ src/com/gray/test/math/findComplement.kt
@@ -0,0 +1,41 @@
+package com.gray.test.math
+
+import kotlin.math.pow
+
+/**
+ * 476. 数字的补数
+ * 给定一个正整数，输出它的补数。补数是对该数的二进制表示取反。
+ * 注意:
+ * 给定的整数保证在32位带符号整数的范围内。
+ * 你可以假定二进制数不包含前导零位。
+ *
+ * 示例 1:
+ * 输入: 5
+ * 输出: 2
+ * 解释: 5的二进制表示为101（没有前导零位），其补数为010。所以你需要输出2。
+ *
+ * 示例 2:
+ * 输入: 1
+ * 输出: 0
+ * 解释: 1的二进制表示为1（没有前导零位），其补数为0。所以你需要输出0。
+ * */
+
+fun findComplement(num: Int): Int {
+    var ret = 0
+    var p = num
+    var digit = 0
+    while (p > 0) {
+        if (p % 2 == 0) {
+            ret += 2.0.pow(digit).toInt()
+        }
+        digit++
+        p = p ushr 1
+    }
+    return ret
+}
+
+fun main() {
+    println(findComplement(5))
+    println(findComplement(1))
+    println(findComplement(4))
+}
