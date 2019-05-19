ca44845eb1eacdc4cee7b7262ba89a34a0f52c9b
gray <emellend@gmail.com>
2019-05-19 21:37:10 +0800

    $(数组): 可被 5 整除的二进制前缀

---
+++ src/com/gray/test/array/prefixesDivBy5.kt
@@ -0,0 +1,45 @@
+package com.gray.test.array
+
+import java.util.*
+
+/***
+ * 1018. 可被 5 整除的二进制前缀
+ * 给定由若干 0 和 1 组成的数组 A。我们定义 N_i：从 A[0] 到 A[i ] 的第 i 个子数组被解释为一个二进制数（从最高有效位到最低有效位）。
+ * 返回布尔值列表 answer，只有当 N_i 可以被 5 整除时，答案 answer[i ] 为 true，否则为 false。
+ *
+ * 示例 1：
+ * 输入：[0,1,1]
+ * 输出：[true,false,false]
+ * 解释：
+ * 输入数字为 0, 01, 011；也就是十进制中的 0, 1, 3 。只有第一个数可以被 5 整除，因此 answer[0] 为真。
+ *
+ * 示例 2：
+ * 输入：[1,1,1]
+ * 输出：[false,false,false]
+ *
+ * 示例 3：
+ * 输入：[0,1,1,1,1,1]
+ * 输出：[true,false,false,false,true,false]
+ *
+ * 示例 4：
+ * 输入：[1,1,1,0,1]
+ * 输出：[false,false,false,false,false]
+ *
+ * 提示：
+ * 1 <= A.length <= 30000
+ * A[i ] 为 0 或 1
+ */
+//n[i] = n[i-1]*2 + A[i]; 所以只要把余数保存下来，再判断余数经过计算后是否为0即可
+fun prefixesDivBy5(A: IntArray): BooleanArray {
+    val ret = BooleanArray(A.size)
+    var mod = 0
+    for (i in 0 until A.size) {
+        mod = (mod * 2 + A[i]) % 5
+        ret[i] = mod == 0
+    }
+    return ret
+}
+
+fun main() {
+    println(Arrays.toString(prefixesDivBy5(intArrayOf(1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1))))
+}
